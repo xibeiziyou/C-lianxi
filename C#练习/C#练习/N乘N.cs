04749eda225abc6afc40969e1f9ab89da99523e4
西贝子游 <jiaziyou8@qq.com>
2024-11-20 21:34:48 +0800

    乘法表

---
+++ C#练习/C#练习/N乘N.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_练习
+{
+    internal class N乘N
+    {
+        //打印乘法表
+        public static int[,] MultiplicationTable(int size)
+        {
+            int[,] 大 = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++) 
+                {
+                    大[i,j] = (i + 1) * (j + 1);
+                } 
+            }
+            return 大;//your table
+        }
+    }
+}
--- C#练习/C#练习/obj/Debug/net8.0/C#练习.AssemblyInfo.cs
+++ C#练习/C#练习/obj/Debug/net8.0/C#练习.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#练习")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b773f756668bf04b3341f1e5350e1a5499493950")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#练习")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#练习")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
